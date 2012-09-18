d32647f5f9c3a4de0ad0ff48bb9b6ae968fdffbf
compuman87 <compuman87@gmail.com>
2012-09-18 12:59:52 -0400

    Added text box for the speed and label. Also started the code behind which will call the gps and get the current speed.
    ** GPS call Needs to be implemented in a loop, not sure until I test *

--- Cardio_v1/src/com/example/cardio_v1/DisplayMessageActivity.java
+++ Cardio_v1/src/com/example/cardio_v1/DisplayMessageActivity.java
@@ -2,11 +2,23 @@ package com.example.cardio_v1;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.widget.TextView;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 
-public class DisplayMessageActivity extends Activity {
+
+public class DisplayMessageActivity extends Activity 
+{
+	private String speed;
+	private TextView textView3;
+	private LocationManager locationManager;
+	private String provider;
+	private String location1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,8 @@ public class DisplayMessageActivity extends Activity {
         //TextView textView2 = new TextView(this);
         TextView textView1 = (TextView) findViewById(R.id.actSpeed);
         TextView textView2 = (TextView) findViewById(R.id.actTrack);
+        textView3 = (TextView) findViewById(R.id.yourSpeed);
+        
         		
         
         
@@ -30,11 +44,35 @@ public class DisplayMessageActivity extends Activity {
         textView2.setText(message2);
         //setContentView(textView);
         
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setSpeedRequired(true);
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+        
+        
+        if (location!=null)
+        {
+        	locationChanged(location);
+        }
+        else
+        {
+        
+        }
+        
+        
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_display_message, menu);
         return true;
+    }
+    
+    private void locationChanged(Location location)
+    {
+    	speed = Float.toString(location.getSpeed());
+    	textView3.setText(speed);
+    	
     }
 }
