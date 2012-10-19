5db4c16e50581eb7a2938d45d172bbbbfece0985
Doug Morgan <dmorgan132333@yahoo.com>
2012-10-19 09:39:52 -0400

    Speed is calculated and shows up on GUI

--- Cardio_v1/src/com/example/cardio_v1/GPSservice.java
+++ Cardio_v1/src/com/example/cardio_v1/GPSservice.java
@@ -18,6 +18,14 @@ public class GPSservice extends Service {
 	private LocationListener ll;
 	private final IBinder bind = new gpsbinder();
 	private float speed;
+	long starttime = 0;
+	float time = starttime;//60000;
+	double startLong = 0;
+	double startLat = 0;
+	double endLong;
+	double endLat;
+	int count = 0;
+	Location start = null;
 	
 	//Create inner Binder Class
 	public class gpsbinder extends Binder {
@@ -52,18 +60,62 @@ public class GPSservice extends Service {
 	private class LocListener implements LocationListener {
 		public void onLocationChanged(Location l) {
 			if (l != null) {
+				
 				//To test getSpeed - UNCOMMENT the following line, run the program and send a location to the emulator and it should show up
 				//meters per second
-				l.setSpeed(28); 
-				speed = l.getSpeed();
+				//l.setSpeed(28); 
+				//speed = l.getSpeed();
 			    
 				//To Test location changes --UNCOMMENT the following line and run
 				//Toast.makeText(getBaseContext(), "Location:  Lat: " + l.getLatitude() + "Long: " + l.getLongitude(), Toast.LENGTH_SHORT).show();
 				//Potentially good method to compute total distance traveled
 				//l.distanceBetween(startLatitude, startLongitude, endLatitude, endLongitude, results)
+				makeUseOfNewLocation(l);
 			}
 		}
-		
+		public void makeUseOfNewLocation(Location location){
+    		count++;
+    		if(count>6){
+    			float[] result = new float[3];
+    			if(startLong == 0 && startLat == 0){
+    				//array[0] = location.getLongitude();
+    				start = location;
+    				startLong = location.getLongitude();
+    				startLat = location.getLatitude();
+    				return;
+    			}
+    			else{
+    				endLong = location.getLongitude();
+    				endLat = location.getLatitude();
+    				float previoustime = time/(1000*60*60);
+    				time = time+1000;
+    				float currenttime = time/(1000*60*60);
+    				Location dest = location;
+    				//float disTo = start.distanceTo(dest);
+    			/**distance divided by time get time in hours*/
+    				Location.distanceBetween(startLat, startLong, endLat, endLong, result);
+    			//System.out.println(start.getLatitude(),start.getLongitude(), end.getLatitude() , end.getLongitude());
+    				//System.out.println("disTo.................................." + disTo);
+    				for(int i = 0; i <result.length-1;i++){
+    				//if(result[i] != null){
+    					System.out.println("time hours" + currenttime + " result " +result[0]+ " conversion "+ (result[0]/1609.3));
+    					//System.out.println("i " + i + " result " +result[i]);
+    				//}
+    				}
+    				System.out.printf("time hours %f \n",currenttime);
+    				//result in miles .1*10 to make it 1mile / time in hours
+    				double currentSpeed = (result[0]/1609.3)/((currenttime-previoustime));
+    				speed = (float) currentSpeed;
+    			//sets the starting position to the ending positions
+    				start = dest;
+    				startLong = location.getLongitude();
+    				startLat = location.getLatitude();
+    				//textSpeed.setText(Double.toString(currentSpeed));
+    			//textSpeed.setText(Long.toString(starttime));
+    			}
+    		}
+    		
+    	}
 		public void onProviderDisabled(String provider) {
 			Toast.makeText(getBaseContext(), "GPS Provider is Disabled", Toast.LENGTH_SHORT).show();
 		}
